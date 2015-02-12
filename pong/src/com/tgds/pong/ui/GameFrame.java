f4787011bc43fbe97ee333bd9d0e1c4d70ee6c91
littlewoo <john.littlewood@gmail.com>
2015-02-12 22:58:57 +0000

    Added GameFrame and GamePanel types.

---
+++ pong/src/com/tgds/pong/ui/GameFrame.java
@@ -0,0 +1,26 @@
+/**
+ * File:     GameFrame.java
+ * Project:  pong
+ * 
+ * Copyright Templecombe Game Development Society, 2015.
+ * All rights reserved. 
+ */
+package com.tgds.pong.ui;
+
+import javax.swing.JFrame;
+
+/**
+ * Main frame and method for the game of Pong.
+ * 
+ * @author jdl
+ */
+public class GameFrame extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+
+}
---
+++ pong/src/com/tgds/pong/ui/GamePanel.java
@@ -0,0 +1,19 @@
+/**
+ * File:     GamePanel.java
+ * Project:  pong
+ * 
+ * Copyright Templecombe Game Development Society, 2015.
+ * All rights reserved. 
+ */
+package com.tgds.pong.ui;
+
+import javax.swing.JPanel;
+
+/**
+ * The panel which shows the game.
+ * 
+ * @author jdl
+ */
+public class GamePanel extends JPanel {
+
+}
