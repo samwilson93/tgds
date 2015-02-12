a56de00081130e3711a99d58aa7a6d3e9deab9bd
littlewoo <john.littlewood@gmail.com>
2015-02-12 23:36:29 +0000

    Added initial functionality to GameFrame.

--- pong/src/com/tgds/pong/ui/GameFrame.java
+++ pong/src/com/tgds/pong/ui/GameFrame.java
@@ -9,6 +9,8 @@ package com.tgds.pong.ui;
 
 import javax.swing.JFrame;
 
+import com.tgds.pong.game.Game;
+
 /**
  * Main frame and method for the game of Pong.
  * 
@@ -16,7 +18,28 @@ import javax.swing.JFrame;
  */
 public class GameFrame extends JFrame {
 
+	/** generated UID */
+	private static final long serialVersionUID = 4732876013889337077L;
+
+	/** the panel containing the main game */
+	private final GamePanel gamePanel;
+
 	/**
+	 * Constructor. Build the frame and show it.
+	 */
+	private GameFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Pong");
+
+		gamePanel = new GamePanel(new Game());
+		getContentPane().add(gamePanel);
+		pack();
+		setVisible(true);
+	}
+
+	/**
+	 * Main method for running the game.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
