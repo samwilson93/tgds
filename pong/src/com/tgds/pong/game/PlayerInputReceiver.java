4c882e9fba64f9f19a921b3bcaf5a6f7d44c1a50
littlewoo <john.littlewood@gmail.com>
2015-02-14 01:06:28 +0000

    Added PlayerInputReceiver interface to handle player input.

---
+++ pong/src/com/tgds/pong/game/PlayerInputReceiver.java
@@ -0,0 +1,31 @@
+/**
+ * File:     PlayerInputReceiver.java
+ * Project:  pong
+ * 
+ * Copyright Templecombe Game Development Society, 2015.
+ * All rights reserved. 
+ */
+package com.tgds.pong.game;
+
+/**
+ * Receiver for player input. Any player will call these methods in order to
+ * control a paddle.
+ * 
+ * @author jdl
+ */
+public interface PlayerInputReceiver {
+
+	/**
+	 * receive a movement command and respond to it.
+	 * 
+	 * @param direction the direction of the movement
+	 */
+	void movePaddle(Direction direction);
+
+	/**
+	 * Directions for moving a paddle in
+	 */
+	public enum Direction {
+
+	}
+}
