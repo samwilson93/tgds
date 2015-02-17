b7b7fdfded4419f1c65ff27ad2951b1b38e58bb3
littlewoo <john.littlewood@gmail.com>
2015-02-17 12:10:03 +0000

    Implemented input handling.

---
+++ pong/src/com/tgds/pong/commands/PaddleMoveCommand.java
@@ -0,0 +1,39 @@
+/**
+ * File:     PaddleMoveCommand.java
+ * Project:  pong
+ * 
+ * Copyright Templecombe Game Development Society, 2015.
+ * All rights reserved. 
+ */
+package com.tgds.pong.commands;
+
+import com.tgds.pong.commands.PlayerInputReceiver.Direction;
+import com.tgds.pong.game.Player;
+
+/**
+ * @author jdl
+ */
+public class PaddleMoveCommand extends Command {
+
+	/** the direction of the movement */
+	private final Direction direction;
+
+	/**
+	 * @param player
+	 */
+	public PaddleMoveCommand(Player player, Direction direction) {
+		super(player);
+		this.direction = direction;
+	}
+
+	/**
+	 * Execute the movement command, by ordering the player input receiver to
+	 * move the paddle in the relevant direction.
+	 */
+	@Override
+	public void execute() {
+		System.out.println("Executing player move command: Player="
+		        + getPlayer() + ", direction=" + direction);
+		getPlayer().getInputReceiver().movePaddle(direction);
+	}
+}
